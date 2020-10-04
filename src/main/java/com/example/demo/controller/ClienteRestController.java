aa179870de05249dcbf7bd9350f456131e1f52f8
emirluna <luis.emir.lm@gmail.com>
2020-10-03 23:49:11 -0500

    Handler error for updating a Cliente.

--- src/main/java/com/example/demo/controller/ClienteRestController.java
+++ src/main/java/com/example/demo/controller/ClienteRestController.java
@@ -68,7 +68,6 @@ public class ClienteRestController {
 	}
 	
 	@PostMapping("/clientes")
-	@ResponseStatus(HttpStatus.CREATED)
 	public ResponseEntity<?> create(@RequestBody Cliente cliente) {
 		/*
 		 * On this method was added constrains in the entity to be more realistic
@@ -95,15 +94,42 @@ public class ClienteRestController {
 
 	
 	@PutMapping("/clientes/{id}")
-	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente update(@RequestBody Cliente cliente, @PathVariable Long id) {
+	public ResponseEntity<?> update(@RequestBody Cliente cliente, @PathVariable Long id) {
+		/*
+		 * For this method is a combination between the get and create
+		 * first the validation of finding the Cliente and the right updating
+		 * of the same
+		 */
+		Cliente clienteUpdated =null;
+		Map<String, Object> response = new HashMap<>();
+		
+		
 		Cliente clienteActual = clienteService.findById(id);
 		
-		clienteActual.setName(cliente.getName());
-		clienteActual.setLastName(cliente.getLastName());
-		clienteActual.setEmail(cliente.getEmail());
+		if(clienteActual == null) {
+			response.put("mensaje", "Error: no se pudo editar el cliente con el ID 	: ".concat(id.toString().concat(" No existe en la base de datos!")));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
 		
-		return clienteService.save(clienteActual);
+		}
+
+		try {
+			clienteActual.setName(cliente.getName());
+			clienteActual.setLastName(cliente.getLastName());
+			clienteActual.setEmail(cliente.getEmail());
+			clienteUpdated = clienteService.save(clienteActual);
+			
+		}catch(DataAccessException e) {
+			response.put("mensaje", "Error al actualizar el Cliente en el servidor");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		
+		}
+		
+		response.put("mensaje", "Cliente ha sido Actualizado con éxito");
+		response.put("cliente", clienteUpdated);
+		
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED); 
+				
 	}
 	
 	
